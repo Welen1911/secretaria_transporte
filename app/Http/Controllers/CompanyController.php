bc2e48acbaf388136da1dcbeb7f125b9f701f7c4
Welen1911 <welenalmeida2210@gmail.com>
2024-07-22 09:11:53 -0300

    Feito: crud de company parcial

---
+++ app/Http/Controllers/CompanyController.php
@@ -0,0 +1,76 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\Models\Company;
+use Illuminate\Http\Request;
+
+class CompanyController extends Controller
+{
+    /**
+     * Display a listing of the resource.
+     */
+    public function index()
+    {
+        $companies = Company::all();
+
+        return response(['companies' => $companies], 200);
+    }
+
+    /**
+     * Store a newly created resource in storage.
+     */
+    public function store(Request $request)
+    {
+        $company = Company::create($request->all());
+
+
+        return response(['company' => $company], 201);
+    }
+
+    /**
+     * Display the specified resource.
+     */
+    public function show(string $id)
+    {
+        $company = Company::find($id);
+
+        if (!$company) {
+            return response('', 404);
+        }
+
+        return response(['company' => $company], 200);
+    }
+
+    /**
+     * Update the specified resource in storage.
+     */
+    public function update(Request $request, string $id)
+    {
+        $company = Company::find($id);
+
+        if (!$company) {
+            return response('', 404);
+        }
+
+        $company->update($request->all());
+
+        return response(['company' => $company], 200);
+    }
+
+    /**
+     * Remove the specified resource from storage.
+     */
+    public function destroy(string $id)
+    {
+        $company = Company::find($id);
+
+        if (!$company) {
+            return response('', 404);
+        }
+
+        $company->delete();
+
+        return response(['company' => $company], 200);
+    }
+}
